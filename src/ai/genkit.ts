4be7cb11a41fca6998047e9303ac405afc87423e
Vino Chelo <vinochelo1986@gmail.com>
2025-07-07 20:55:29 +0000

    que otras versiones podemos usar?

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
-export const geminiFlash = googleAI.model('gemini-1.5-flash-latest');
+export const geminiFlash = googleAI.model('gemini-1.5-pro-latest');
 export const geminiImage = googleAI.model(
   'gemini-2.0-flash-preview-image-generation'
 );
