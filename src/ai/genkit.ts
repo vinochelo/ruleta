5fa6782438a7f5fd7a1db82ecdafb9d22c0ce9a0
Vino Chelo <vinochelo1986@gmail.com>
2025-09-02 12:11:34 +0000

    creo que seria mejor solo usar gemini flash la version mas reciente ya q

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,7 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const geminiFlash = googleAI.model('gemini-1.5-flash-latest');
-export const geminiPro = googleAI.model('gemini-1.5-pro-latest');
 export const geminiImage = googleAI.model(
   'gemini-2.0-flash-preview-image-generation'
 );
